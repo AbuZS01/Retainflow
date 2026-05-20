fc1c2480c72f398d8906b05f61e92a2e7f9664b1
AbuZS01 <amir_15@hotmail.co.uk>
2026-05-20 14:28:08 +0100

    feat: switch to light yellow colour scheme

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retainflow-v3';
+const CACHE = 'retainflow-v4';
 const SHELL = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
