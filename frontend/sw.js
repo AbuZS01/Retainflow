872dcd24fc6c2af78125b2555d3ec757673f659f
AbuZS01 <amir_15@hotmail.co.uk>
2026-06-06 17:05:55 +0100

    fix: update hardcoded Fly.io URL to retainflow.onrender.com; bump SW v42

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retainflow-v41';
+const CACHE = 'retainflow-v42';
 const SHELL = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
