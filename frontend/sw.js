f4109dcd8cca9d6f0cffc0d2a093a9c7516938f8
AbuZS01 <amir_15@hotmail.co.uk>
2026-06-06 08:45:09 +0100

    chore: bump SW cache to v34

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retainflow-v33';
+const CACHE = 'retainflow-v34';
 const SHELL = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
